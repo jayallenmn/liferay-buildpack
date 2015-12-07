f5df8f799902976c5590ad8b30b3adf90824b6e6
Luc Desrosiers <ldesrosi@ca.ibm.com>
2015-12-06 23:10:07 -0500

    Commented out unzip

--- lib/java_buildpack/container/tomcat/liferay/liferay_portal.rb
+++ lib/java_buildpack/container/tomcat/liferay/liferay_portal.rb
@@ -51,9 +51,9 @@ module JavaBuildpack
 
       def expand(file)
         with_timing "Expanding #{@component_name} to #{@droplet.sandbox}" do
-          FileUtils.mkdir_p "#{@droplet.sandbox}"
           shell "tar xzf #{file.path} -C #{@droplet.sandbox} 2>&1"
-          shell "unzip -qq #{@droplet.sandbox}/liferay-portal-6.2-ce-ga5.war -d #{@droplet.sandbox}/webapps/ROOT 2>&1"
+          print "Expanding Liferay WAR"
+          #shell "unzip -qq #{@droplet.sandbox}/liferay-portal-6.2-ce-ga5.war -d #{@droplet.sandbox}/webapps/ROOT 2>&1"
         end
       end
 
